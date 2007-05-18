3a253d6a97942d34222824257c409292de593dbf
lilarrateguy <lilarrateguy@134011fe-ee2e-0410-afaa-8fa9f0f2941b>
2007-05-18 14:10:09 +0000

    más commit. Ahora podemos ver cada vez que se cumple el objetivo, bajo que condiciones.

--- tp1/src/tpsia/tp1/agente/ObjetivoTP.java
+++ tp1/src/tpsia/tp1/agente/ObjetivoTP.java
@@ -1,5 +1,7 @@
 package tpsia.tp1.agente;
 
+import org.apache.log4j.Logger;
+
 import tpsia.tp1.acciones.AvanzarAbajo;
 import tpsia.tp1.acciones.Pelear;
 import tpsia.tp1.busqueda.Nodo;
@@ -19,29 +21,61 @@ public class ObjetivoTP implements IObjetivo {
 	}
 	
 	public boolean cumpleObjetivo(Estado estado) {
-		if (estado.getEnergia() <= 0)
-			return false;
+		boolean cumplio = false;
+		boolean convienePelear,convieneMoverse,conoceTodo,hayAlimentos,hayEnemigos;
 		
-		boolean convienePelear = estado.getEnergia() >
-			estado.getPromedioVarEnergia(Pelear.getInstancia());
-		boolean convieneMoverse = estado.getEnergia() >
-			estado.getPromedioVarEnergia(AvanzarAbajo.getInstancia());
+		Logger log = Logger.getLogger("Pacman.Busqueda.Objetivo");
 		
-		if (estado.getAmbiente().conoceTodo()) {
-			if (convieneMoverse &&
-					(estado.getAmbiente().hayAlimentosSinComer() ||
-							(estado.getAmbiente().hayEnemigosVivos() && convienePelear)))
-				return false;
-			else
-				return true;
-		}
-		else {
-			if (convieneMoverse)
-				return false;
-			else
-				return true;
-		}
+		if (estado.getEnergia() <= 0) {
+			log.debug("El Pacman está muerto");
+			cumplio = false;
+		} else {
+			convienePelear = estado.getEnergia() >
+				estado.getPromedioVarEnergia(Pelear.getInstancia());
+			convieneMoverse = estado.getEnergia() >
+				estado.getPromedioVarEnergia(AvanzarAbajo.getInstancia());			
+			conoceTodo = estado.getAmbiente().conoceTodo();
+			hayAlimentos = estado.getAmbiente().hayAlimentosSinComer();
+			hayEnemigos = estado.getAmbiente().hayEnemigosVivos();
+
 			
+			
+			// Condicion del objetivo
+			boolean cumplio1 = conoceTodo && !convieneMoverse;
+			boolean cumplio2 = conoceTodo && convieneMoverse && !hayAlimentos && !(hayEnemigos && convienePelear);
+			boolean cumplio3 = !conoceTodo && !convieneMoverse;
+
+			cumplio = cumplio1 || cumplio2 || cumplio3;
+			/*
+			 * Version simplificada:
+			 * cumplio = 
+			 */
+			
+			/*
+			if (conoceTodo) {
+				if (convieneMoverse &&
+						(hayAlimentos ||
+								(estado.getAmbiente().hayEnemigosVivos() && convienePelear))) {
+					cumplio = false;
+				} else {
+					cumplio = true;
+				}
+					
+			} else {
+				if (convieneMoverse) {
+					return false;
+				} else {
+					return true;
+				}
+			}
+			*/
+			if (cumplio) {
+				log.debug("Conviene pelear: "+convienePelear + " Conviene moverse: "+convieneMoverse +
+						" Conoce todo: "+conoceTodo + " Hay alimentos: "+hayAlimentos + " Hay enemigos: "+hayEnemigos);
+			}
+		}
+		
+		return cumplio;
 	}
 
 	public boolean cumpleObjetivo(Nodo nodo) {
