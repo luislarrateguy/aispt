0f67d983c7145369aa19cada71d2acf041a83593
lilarrateguy <lilarrateguy@134011fe-ee2e-0410-afaa-8fa9f0f2941b>
2007-05-20 13:41:22 +0000

    había olvidado subir BusquedaFactory

---
+++ tp1/src/tpsia/tp1/busqueda/BusquedaFactory.java
@@ -0,0 +1,28 @@
+package tpsia.tp1.busqueda;
+
+import java.util.ArrayList;
+import tpsia.tp1.busqueda.*;
+import tpsia.tp1.agente.Estado;
+import tpsia.tp1.agente.IObjetivo;
+import tpsia.tp1.agente.VisionAmbiente;
+
+public class BusquedaFactory {
+	public static Busqueda Create(String tipoBusqueda, Estado estado, IObjetivo objetivo, ArrayList<VisionAmbiente> estadosAlcanzados) {
+		Busqueda b;
+		if (tipoBusqueda.compareToIgnoreCase("aestrella") == 0)
+				b = new BusquedaAEstrella(estado, objetivo, estadosAlcanzados);
+		else if (tipoBusqueda.compareToIgnoreCase("avara") == 0) 
+				b = new BusquedaAvara(estado, objetivo, estadosAlcanzados);
+		else if (tipoBusqueda.compareToIgnoreCase("profundidad") == 0)
+				b = new BusquedaProfundidad(estado, objetivo, estadosAlcanzados);
+		else if (tipoBusqueda.compareToIgnoreCase("costouniforme") == 0)
+			b = new BusquedaCostoUniforme(estado, objetivo, estadosAlcanzados);
+		else if (tipoBusqueda.compareToIgnoreCase("amplitud") == 0)
+			b = new BusquedaAmplitud(estado, objetivo, estadosAlcanzados);
+		else
+			b = null;
+
+		return b;
+		
+	}
+}
