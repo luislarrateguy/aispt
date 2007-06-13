4ffc7eb05cf62787e763376aac3b2b2d20b1e2bc
miltondp <miltondp@134011fe-ee2e-0410-afaa-8fa9f0f2941b>
2007-06-13 22:57:17 +0000

    - Base de conocimiento actualizada.

--- tp2/base_conocimiento.pl
+++ tp2/base_conocimiento.pl
@@ -1,62 +1,77 @@
-percepcion([enemigo,enemigo,vacia,comida],2,2,10,1).
+percepcion([enemigo,comida,vacia,vacia],2,2,10,1).
+accionEjecutada(arriba,1).
+percepcion([enemigo,comida,vacia,vacia],0,0,8,2).
+accionEjecutada(arriba,2).
+percepcion([enemigo,comida,vacia,vacia],0,0,7,3).
+accionEjecutada(arriba,3).
+percepcion([enemigo,comida,vacia,vacia],0,0,2,4).
+accionEjecutada(arriba,4).
 
-%%Estos son los statements que no infieren
-
-%% "Funcion" para sumar posiciones en forma de lista circular
-sumarPosicion(P,O,P1):-O<=0,P=:=1,P1 is 4.
-sumarPosicion(P,O,P1):-O<=0,P=\=1,P1 is P-1.
+%% Funcion probada.
+sumarPosicion(P,O,P1):-O=:=0-1,P=:=1,P1 is 4.
+sumarPosicion(P,O,P1):-O=:=0-1,P=\=1,P1 is P-1.
 sumarPosicion(P,O,P1):-O=:=1,P=:=4,P1 is 1.
 sumarPosicion(P,O,P1):-O=:=1,P=\=4,P1 is P+1.
 
 %%hace la sumatoria de una lista
+%% Funcion probada.
 sumatoria([],0):-!.
 sumatoria([L|Ls],C):-sumatoria(Ls,T),C is T+L.
 
 %%retorna la cantidad de elementos de una lista
 cantidad([],0):-!.
-cantidad([L|Ls],C):-cantidad(Ls,T),C is T+1.
+cantidad([_|Ls],C):-cantidad(Ls,T),C is T+1.
 
 %%calcula el promedio de una lista
-promedio(L,P):-sumatoria(L,Z), cantidad(L,C), P is (S/C).
+%% Funcion probada.
+promedio(L,P):-sumatoria(L,S), cantidad(L,C), P is (S/C).
 
+%% Funcion probada.
+posicion(X,Y,1):-percepcion([_,_,_,_],X,Y,_,1).
+
+%% PERCEPCIONES
 %% infiriendo de las percepciones
-enemigo(X1,Y,S):-percepcion([enemigo,_,_,_],X,Y,_,S),sumarPosicion(X,-1,X1).
-enemigo(X1,Y,S):-percepcion([_,enemigo,_,_],X,Y,_,S),sumarPosicion(X,1,X1).
-enemigo(X,Y1,S):-percepcion([_,_,enemigo,_],X,Y,_,S),sumarPosicion(Y,-1,Y1).
-enemigo(X,Y1,S):-percepcion([_,_,_,enemigo],X,Y,_,S),sumarPosicion(Y,1,Y1).
+enemigo(X,Y,S):-percepcion([enemigo,_,_,_],_,_,_,S),adyacente(X,Y,izquierda,S).
+enemigo(X,Y,S):-percepcion([_,enemigo,_,_],_,_,_,S),adyacente(X,Y,derecha,S).
+enemigo(X,Y,S):-percepcion([_,_,enemigo,_],_,_,_,S),adyacente(X,Y,arriba,S).
+enemigo(X,Y,S):-percepcion([_,_,_,enemigo],_,_,_,S),adyacente(X,Y,abajo,S).
 
-comida(X1,Y,S):-percepcion([comida,_,_,_],X,Y,_,S),sumarPosicion(X,-1,X1).
-comida(X1,Y,S):-percepcion([_,comida,_,_],X,Y,_,S),sumarPosicion(X,1,X1).
-comida(X,Y1,S):-percepcion([_,_,comida,_],X,Y,_,S),sumarPosicion(Y,-1,Y1).
-comida(X,Y1,S):-percepcion([_,_,_,comida],X,Y,_,S),sumarPosicion(Y,1,Y1).
+comida(X,Y,S):-percepcion([comida,_,_,_],_,_,_,S),adyacente(X,Y,izquierda,S).
+comida(X,Y,S):-percepcion([_,comida,_,_],_,_,_,S),adyacente(X,Y,derecha,S).
+comida(X,Y,S):-percepcion([_,_,comida,_],_,_,_,S),adyacente(X,Y,arriba,S).
+comida(X,Y,S):-percepcion([_,_,_,comida],_,_,_,S),adyacente(X,Y,abajo,S).
 
-vacia(X1,Y,S):-percepcion([vacia,_,_,_],X,Y,_,S),sumarPosicion(X,-1,X1).
-vacia(X1,Y,S):-percepcion([_,vacia,_,_],X,Y,_,S),sumarPosicion(X,1,X1).
-vacia(X,Y1,S):-percepcion([_,_,vacia,_],X,Y,_,S),sumarPosicion(Y,-1,Y1).
-vacia(X,Y1,S):-percepcion([_,_,_,vacia],X,Y,_,S),sumarPosicion(Y,1,Y1).
+vacia(X,Y,S):-percepcion([vacia,_,_,_],_,_,_,S),adyacente(X,Y,izquierda,S).
+vacia(X,Y,S):-percepcion([_,vacia,_,_],_,_,_,S),adyacente(X,Y,derecha,S).
+vacia(X,Y,S):-percepcion([_,_,vacia,_],_,_,_,S),adyacente(X,Y,arriba,S).
+vacia(X,Y,S):-percepcion([_,_,_,vacia],_,_,_,S),adyacente(X,Y,abajo,S).
 
-posicion(X,Y,S):-percepcion([_,_,_,_],X,Y,_,S).
 
+%% Funcion probada.
 energia(E,S):-percepcion([_,_,_,_],_,_,E,S).
+
 
 %%reglas causales
 convieneMoverse(si,S):- energia(E,S), promedioPorMoverse(P,S), (E + P) > 0.
-convieneMoverse(no,S):- energia(E,S), promedioPorMoverse(P,S), (E + P) <= 0.
+convieneMoverse(no,S):- energia(E,S), promedioPorMoverse(P,S), (E + P) =< 0.
 convienePelear(si,S) :- energia(E,S), promedioPorPelear(P,S), (E + P) > 0.
-convienePelear(no,S) :- energia(E,S), promedioPorPelear(P,S), (E + P) <= 0.
+convienePelear(no,S) :- energia(E,S), promedioPorPelear(P,S), (E + P) =< 0.
 
+
+%% Funcion probada.
 conoce(X,Y,S) :- vacia(X,Y,S).
 conoce(X,Y,S) :- comida(X,Y,S).
 conoce(X,Y,S) :- enemigo(X,Y,S).
 
 
-adyacente(X1,y,derecha,S)  :- posicion(X,Y,S),sumarPosicion(X,1,X1).
-adyacente(X1,y,izquierda,S):- posicion(X,Y,S),sumarPosicion(X,-1,X1).
+adyacente(X1,Y,derecha,S)  :- posicion(X,Y,S),sumarPosicion(X,1,X1).
+adyacente(X1,Y,izquierda,S):- posicion(X,Y,S),sumarPosicion(X,-1,X1).
 adyacente(X,Y1,arriba,S)   :- posicion(X,Y,S),sumarPosicion(Y,1,Y1).
 adyacente(X,Y1,abajo,S)    :- posicion(X,Y,S),sumarPosicion(Y,-1,Y1).
 
-datosEnergia(_,[],0):-!.
-datosEnergia(A,[E2|Es],S):-accionEjecutada(A,S), energia(E1,S), S1 is S-1,energia(E0,S1),E2 is (E0 - E1).
+%% Funcion probada.
+datosEnergia(_,[],1):-!.
+datosEnergia(A,[D|Ds],S1):-S0 is S1-1,datosEnergia(A,Ds,S0),energia(E1,S1),energia(E0,S0),D is (E0 - E1).
 
 %% todo esto se podria hacer en java
 %% el llevar las energias y promedios
@@ -64,8 +79,11 @@ datosEnergia(A,[E2|Es],S):-accionEjecutada(A,S), energia(E1,S), S1 is S-1,energi
 %% promedioPorPelear(-2,3).
 %% para indicar que en promedio perdio 2 a la situacion 3
 
-promedioPorPelear(P,S):-datosEnergia(pelear,Es),promedio(Es,P).
-promedioPorMoverse(P,S):-datosEnergia(mover,Es),promedio(Es,P).
+promedioPorPelear(0,1):-!.
+promedioPorPelear(P,S):-datosEnergia(pelear,Es,S),promedio(Es,P).
+
+promedioPorMoverse(0,1):-!.
+promedioPorMoverse(P,S):-datosEnergia(mover,Es,S),promedio(Es,P).
 
 %% Para cumplir el objetivo, se me ocurre que basta unicamente
 %% con tableroVacio(S)
@@ -79,20 +97,21 @@ tableroVacio(S):-vacia(1,1,S),vacia(1,2,S),vacia(1,3,S),vacia(1,4,S),
                  vacia(3,1,S),vacia(3,2,S),vacia(3,3,S),vacia(3,4,S),
                  vacia(4,1,S),vacia(4,2,S),vacia(4,3,S),vacia(4,4,S).
                  
+%% DEPRECATED
 %%defino por las dudas cosas de las que hablamos
 %% esta seria una clausula positiva para decir
 %% que no hay enemigo en XY si la conozco
 %% aunque dudo que no este de más poner lo de vacia y comida
-noHayEnemigo(X,Y,S):-vacia(X,Y,S).
-noHayEnemigo(X,Y,S):-comida(X,Y,S).
-noHayEnemigo(X,Y,S):-enemigo(X,Y,S),!,fail.
-noHayEnemigo(X,Y,S).
+%%noHayEnemigo(X,Y,S):-vacia(X,Y,S).
+%%noHayEnemigo(X,Y,S):-comida(X,Y,S).
+%%noHayEnemigo(X,Y,S):-enemigo(X,Y,S),!,fail.
 
+%% DEPRECATED
 %%mismo para comida
-noHayComida(X,Y,S):-vacia(X,Y,S).
-noHayComida(X,Y,S):-enemigo(X,Y,S).
-noHayComida(X,Y,S):-comida(X,Y,S),!,fail.
-noHayComida(X,Y,S).
+%%noHayComida(X,Y,S):-vacia(X,Y,S).
+%%noHayComida(X,Y,S):-enemigo(X,Y,S).
+%%noHayComida(X,Y,S):-comida(X,Y,S),!,fail.
+%%noHayComida(X,Y,S).
 
 %% Esto sería util debido a lo explicado en la documentacion,
 %% pero insisto:
@@ -129,40 +148,55 @@ cumplioObjetivo(S):-condicionUnoObjetivo(S),condicionDosObjetivo(S),condicionTre
 
 acciones([],0):-!.
 acciones([A|As],S):-accionEjecutada(A,S),S1 is S-1,acciones(As,S1).
+accionMover(S):-accionEjecutada(arriba,S).
+accionMover(S):-accionEjecutada(abajo,S).
+accionMover(S):-accionEjecutada(derecha,S).
+accionMover(S):-accionEjecutada(izquierda,S).
 
 %%ESTADO SUCESOR
+%% Funcion comprobada.
+posicion(X,Y,S1):-S is S1-1,accionEjecutada(comer,S),posicion(X,Y,S).
+posicion(X,Y,S1):-S is S1-1,accionEjecutada(pelear,S),posicion(X,Y,S).
 
-%% El estado sucesor para la posicion no haría falta
-%% ya que viene como percepcion cada vez.
+%% Atencion: para estudiar. Esto es un HACK feo para que tome varios tipos de consultas
+%% Funcion comprobada.
+posicion(X,Y1,S1):-S is S1-1,accionEjecutada(arriba,S),posicion(X,Y,S),sumarPosicion(Y,-1,Y1).
+posicion(X,Y1,S1):-S is S1-1,accionEjecutada(abajo,S),posicion(X,Y,S),sumarPosicion(Y,1,Y1).
+posicion(X1,Y,S1):-S is S1-1,accionEjecutada(derecha,S),posicion(X,Y,S),sumarPosicion(X,1,X1).
+posicion(X1,Y,S1):-S is S1-1,accionEjecutada(izquierda,S),posicion(X,Y,S),sumarPosicion(X,-1,X1).
 
-vacia(X,Y,S1):-vacia(X,Y,S),!.
-vacia(X,Y,S1):-comida(X,Y,S),posicion(X,Y,S),accionEjecutada(comer,S1),S1 is S + 1,!.
-vacia(X,Y,S1):-enemigo(X,Y,S),posicion(X,Y,S),accionEjecutada(pelear,S1),S1 is S + 1,!.
+%% Funcion comprobada.
+vacia(X,Y,S1):- S1 > 1,S is S1-1,vacia(X,Y,S).
+vacia(X,Y,S1):- S1 > 1,S is S1-1,accionEjecutada(comer,S),posicion(X,Y,S).
+vacia(X,Y,S1):- S1 > 1,S is S1-1,accionEjecutada(pelear,S),posicion(X,Y,S).
 
-comida(X,Y,S1):-comida(X,Y,S),posicion(X1,Y1,S),X=\=X1,Y=\=Y1,!.
-comida(X,Y,S1):-comida(X,Y,S),posicion(X,Y,S),accionEjecutada(mover,S1),S1 is S + 1,!.
+%% Funcion comprobada.
+comida(X,Y,S1):-S1 > 1,S is S1-1,posicion(_,Y1,S),comida(X,Y,S),Y=\=Y1.
+comida(X,Y,S1):-S1 > 1,S is S1-1,posicion(X1,_,S),comida(X,Y,S),X=\=X1.
+comida(X,Y,S1):-S1 > 1,S is S1-1,posicion(X,Y,S),accionMover(S),comida(X,Y,S).
 
-
-enemigo(X,Y,S1):-enemigo(X,Y,S),posicion(X1,Y1,S),X=\=X1,Y=\=Y1,S1 is S + 1,!.
-enemigo(X,Y,S1):-enemigo(X,Y,S),posicion(X,Y,S),accionEjecutada(mover,S1),S1 is S + 1,!.
+%% Funcion comprobada.
+enemigo(X,Y,S1):-S1 > 1,S is S1-1,posicion(_,Y1,S),enemigo(X,Y,S),Y=\=Y1.
+enemigo(X,Y,S1):-S1 > 1,S is S1-1,posicion(X1,_,S),enemigo(X,Y,S),X=\=X1.
+enemigo(X,Y,S1):-S1 > 1,S is S1-1,posicion(X,Y,S),accionMover(S),enemigo(X,Y,S).
 
 
 %%valoracion de las acciones
 excelente(comer,S):-posicion(X,Y,S),comida(X,Y,S).
 
 muy_bueno(pelear,S):-posicion(X,Y,S),enemigo(X,Y,S),convienePelear(si,S).
-muy_bueno(D,S):-posicion(X,Y,S),adyacente(Xa,Ya,D,S),comida(Xa,Ya,S),convieneMoverse(S).
+muy_bueno(D,S):-adyacente(Xa,Ya,D,S),comida(Xa,Ya,S),convieneMoverse(si,S).
 
-bueno(D,S):-posicion(X,Y,S),adyacente(Xa,Ya,D,S),enemigo(Xa,Ya,S),convienePelear(si,S).
+bueno(D,S):-adyacente(Xa,Ya,D,S),enemigo(Xa,Ya,S),convienePelear(si,S).
 
-regular(D,S):-posicion(X,Y,S),adyacente(Xa,Ya,D,S),vacia(Xa,Ya,S),convieneMoverse(si,S).
+regular(D,S):-adyacente(Xa,Ya,D,S),vacia(Xa,Ya,S),convieneMoverse(si,S).
 
 %%podria necesitar pasar, quiza es mas corto
-malo(D,S):-posicion(X,Y,S),adyacente(Xa,Ya,D,S),enemigo(Xa,Ya,S),convienePelear(no,S).
+malo(D,S):-adyacente(Xa,Ya,D,S),enemigo(Xa,Ya,S),convienePelear(no,S).
 
 %% lo lleva al muere
-muy_malo(pelear,S):-posicion(X,Y,S),adyacente(Xa,Ya,D,S),enemigo(Xa,Ya,S),convienePelear(no,S).
-muy_malo(D,S):-posicion(X,Y,S),adyacente(Xa,Ya,D,S),vacia(Xa,Ya,S),convieneMoverse(no,S).
+muy_malo(pelear,S):-posicion(X,Y,S),enemigo(X,Y,S),convienePelear(no,S).
+muy_malo(D,S):-adyacente(Xa,Ya,D,S),vacia(Xa,Ya,S),convieneMoverse(no,S).
 
 
 mejorAccion(X,S):-excelente(X,S).
@@ -170,4 +204,7 @@ mejorAccion(X,S):-muy_bueno(X,S).
 mejorAccion(X,S):-bueno(X,S).
 mejorAccion(X,S):-regular(X,S).
 mejorAccion(X,S):-malo(X,S).
-mejorAccion(X,S):-muy_malo(X,S).
+mejorAccion(X,S):-muy_malo(X,S),fail.
+
+
+
