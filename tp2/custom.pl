2e4195313b94ba9912e9fa2452b28ef3f791076a
lilarrateguy <lilarrateguy@134011fe-ee2e-0410-afaa-8fa9f0f2941b>
2007-06-15 19:28:11 +0000

    Anda con custom. Es medio inutil pero anda..

---
+++ tp2/custom.pl
@@ -0,0 +1,7 @@
+percepcion([vacia,comida,vacia,enemigo],1,1,10,1).
+%% recomendacion de una ejecucion de mejor accion con query
+accionEjecutada(derecha,1).
+percepcion([vacia,vacia,vacia,vacia],2,1,9,2).
+accionEjecutada(comer,2).
+percepcion([vacia,vacia,vacia,vacia],2,1,12,3).
+
---
+++ tp2/test_custom.pl
@@ -0,0 +1,5 @@
+%% aca esta medio raro. Iria en la iteracion
+posicion(X,Y,1).
+
+mejorAccion(X,1).
+
